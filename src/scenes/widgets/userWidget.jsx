3158d77dd0be5ba1b3002be32025b28ae7608f20
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-23 21:55:02 -0500

    Update userWidget.jsx

--- src/scenes/widgets/userWidget.jsx
+++ src/scenes/widgets/userWidget.jsx
@@ -19,7 +19,7 @@ const UserWidget = ({ userId, picturePath }) => {
     const main = palette.neutral.main;
 
     const getUser = async () => {
-        const response = await fetch(`https://devconnect-api-5vst.onrender.com/users/${userId}`, {
+        const response = await fetch(`https://devconnect-api.adaptable.app/users/${userId}`, {
             method: "GET", 
             headers: { Authorization: `Bearer ${token}`}       
         });
