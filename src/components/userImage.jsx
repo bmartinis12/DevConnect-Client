5e34beee402417c9a33c433e4afe2cfbc58cd58c
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-23 21:51:09 -0500

    Update userImage.jsx

--- src/components/userImage.jsx
+++ src/components/userImage.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 const UserImage = ({ image, size = '60px' }) => {
     return (
         <Box width={size} height={size}>
-            <img style={{ objectFit: 'cover', borderRadius: '50%' }} width={size} height={size} alt='user' src={`https://devconnect-api-5vst.onrender.com/assets/${image}`} />
+            <img style={{ objectFit: 'cover', borderRadius: '50%' }} width={size} height={size} alt='user' src={`https://devconnect-api.adaptable.app/assets/${image}`} />
         </Box>
     )
 };
