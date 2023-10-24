5156e7dc9dbd1d2e7462fc9d9e2a2c0cac2349a8
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-10-23 21:52:29 -0500

    Update index.jsx

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
     const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
 
     const getUser = async () => {
-        const response = await fetch(`https://devconnect-api-5vst.onrender.com/users/${userId}`, {
+        const response = await fetch(`https://devconnect-api.adaptable.app/users/${userId}`, {
             method: 'GET',
             headers: { Authorization: `Bearer ${token}` }
         });
